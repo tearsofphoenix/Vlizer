841f6584093825638ff0f4b2b0724925bfabdc9c
tearsofphoenix <tearsofphoenix@icloud.com>
2014-03-20 15:04:50 +0800

    working on.

---
+++ QGameiOS/VZTheme.h
@@ -0,0 +1,13 @@
+//
+//  VZTheme.h
+//  Vlizer
+//
+//  Created by Mac003 on 14-3-20.
+//  Copyright (c) 2014年 Mac003. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface VZTheme : NSObject
+
+@end
---
+++ QGameiOS/VZTheme.m
@@ -0,0 +1,13 @@
+//
+//  VZTheme.m
+//  Vlizer
+//
+//  Created by Mac003 on 14-3-20.
+//  Copyright (c) 2014年 Mac003. All rights reserved.
+//
+
+#import "VZTheme.h"
+
+@implementation VZTheme
+
+@end
