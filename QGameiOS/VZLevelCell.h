a3fc1503db663ac6c941357f8a70e3265bd60c65
tearsofphoenix <tearsofphoenix@icloud.com>
2014-03-20 16:13:05 +0800

    working on.

---
+++ QGameiOS/VZLevelCell.h
@@ -0,0 +1,15 @@
+//
+//  VZLevelCell.h
+//  Vlizer
+//
+//  Created by Mac003 on 14-3-20.
+//  Copyright (c) 2014年 Mac003. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#define VZCellID @"view.collection.cell"
+
+@interface VZLevelCell : UICollectionViewCell
+
+@end
---
+++ QGameiOS/VZLevelCell.m
@@ -0,0 +1,24 @@
+//
+//  VZLevelCell.m
+//  Vlizer
+//
+//  Created by Mac003 on 14-3-20.
+//  Copyright (c) 2014年 Mac003. All rights reserved.
+//
+
+#import "VZLevelCell.h"
+
+@implementation VZLevelCell
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self)
+    {
+        // Initialization code
+    }
+    return self;
+}
+
+
+@end
